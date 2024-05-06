0018ce5df844827b4eb4b7bad4220e283b6cd32b
rjimenezg73 <106135874+rjimenezg73@users.noreply.github.com>
2024-05-06 00:07:50 -0600

    Tipos de datos en Java

---
+++ Varios/TiposDatos.java
@@ -0,0 +1,25 @@
+
+public class TiposDatos {
+  public static void main(String[] args) {
+    int num1 = 9;
+    byte by = 127;
+    short sh = 558;
+    long l = 5854l;
+    float f = 5.8f;
+    double d = 5.8;
+    char c = 'k';
+    char c2 = '8';
+    boolean b = true;
+
+
+    System.out.println(num1);
+    System.out.println(by);
+    System.out.println(sh);
+    System.out.println(l);
+    System.out.println(f);
+    System.out.println(d);
+    System.out.println(c);
+    System.out.println(c2);
+    System.out.println(b);
+  }
+}
