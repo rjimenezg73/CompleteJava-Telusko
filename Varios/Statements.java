6629bb79edb2f2f5119eec1483c7eaed36d9442a
rjimenezg73 <106135874+rjimenezg73@users.noreply.github.com>
2024-05-07 22:46:26 -0600

    Statements if

---
+++ Varios/Statements.java
@@ -0,0 +1,12 @@
+public class Statements {
+
+  public static void main(String[] args) {
+    int x = 18;
+
+    if(x < 10){
+      System.out.println("Hola");
+    }else{
+      System.out.println("Adios");
+    }
+  }
+}
